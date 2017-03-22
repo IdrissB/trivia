a84d65260ba6529535b323e1bbba0744cc9a1676
BOUHASSOUN IDRISS p1004755 <p1004755@univ-lyon1.fr>
2017-03-22 09:17:48 +0100

    Ajout Classe Player - Nom

---
+++ C#/Trivia/Trivia/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Trivia
+{
+    class Player
+    {
+        private string name;
+
+        public Player(string name)
+        {
+            this.Name = name;
+        }
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
