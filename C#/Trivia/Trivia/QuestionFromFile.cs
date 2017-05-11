e31efbf6c6431ee483e516a617c03a44f32e9886
BOUHASSOUN IDRISS p1004755 <p1004755@univ-lyon1.fr>
2017-05-11 17:34:33 +0200

    Add class QuestionFromFile

---
+++ C#/Trivia/Trivia/QuestionFromFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trivia
+{
+    class QuestionFromFile : IQuestionsRepository
+    {
+
+        public LinkedList<string> GetQuestions(string category)
+        {
+            // Read the file as one string.
+            System.IO.StreamReader myFile =
+            new System.IO.StreamReader("c:\\test.txt");
+            string myString = myFile.ReadToEnd();
+
+            myFile.Close();
+
+        }
+    }
+}
